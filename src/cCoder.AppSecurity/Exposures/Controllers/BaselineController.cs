7717a377a2b2cca64239bbb74abdaa40d8491c88
Paul Ward <paul.ward@ccoder.co.uk>
2026-07-07 14:31:19 +0100

    Add AppSecurity baseline endpoint (#20)
    * Add AppSecurity baseline endpoint
    
    * Add UI baseline package payloads
    
    * Split UI baseline packages into partials

---
+++ src/cCoder.AppSecurity/Exposures/Controllers/BaselineController.cs
@@ -0,0 +1,13 @@
+using cCoder.AppSecurity.Exposures.Setup;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cCoder.AppSecurity.Exposures.Controllers;
+
+[ApiController]
+[Route("Api/AppSecurity/Baseline")]
+public sealed class BaselineController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() =>
+        Ok(UIBaseline.Packages);
+}
---
+++ src/cCoder.AppSecurity/Exposures/Setup/UIBaseline.cs
@@ -0,0 +1,14 @@
+using cCoder.Data.Models.Packaging;
+
+namespace cCoder.AppSecurity.Exposures.Setup;
+
+public static partial class UIBaseline
+{
+    public static Package[] Packages => [
+        Components,
+        Pages,
+        Resources,
+        Roles,
+        PageRoles
+    ];
+}
